e8fee7d93e07790afac3168af7e55e65ece96ffb
LorenaCcosta <lorenaaraujo954@gmail.com>
2024-11-16 21:57:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
+const displayNoneForm = document.getElementById("entrar-conta");
+const displayNoneBackground = document.getElementById("div-cor-background");
+const displayBlockForm = document.getElementById("entrar-conta");
+const displayBlockBackground = document.getElementById("div-cor-background");
+
 //Ao clicar em Saiba Mais, irá rolar até ao final da página
 function saibaMaisScroll(){
-    var link1 = document.querySelector("h2");
-    link1.scrollIntoView({behavior: 'smooth'});
+  var link1 = document.querySelector("h2");
+  link1.scrollIntoView({behavior: 'smooth'});
 
   }
 // Abrir o formulario para entrar
 function abrirForm(){
-  document.getElementById("entrar-conta").style.display = "block";
-  document.getElementById("div-cor-background").style.display = "block";
+  displayBlockForm.style.display = "block";
+  displayBlockBackground.style.display = "block";
   document.body.style.overflow = "hidden";
 }
 //Fechar o formulario apertando no X
 function fecharForm(){
-  document.getElementById("entrar-conta").style.display = "none"
-  document.getElementById("div-cor-background").style.display = "none";
+  displayNoneForm.style.display = "none"
+  displayNoneBackground.style.display = "none";
   document.body.style.overflow = "auto"
 }
 //Ao clicar no checkbox a senha do input Password ira aparecer em texto
 function mostrarSenha(){
   const senhaInput = document.getElementById('senha-div-entrar');
   const checkBox = document.getElementById('mostrar-senha-checkbox');
+
     if (checkBox.checked) {
       senhaInput.type = 'text';
       
@@ -38,11 +44,12 @@ function btnEntrar(event){
   const senhaInput = document.getElementById('senha-div-entrar').value;
 
   if (senhaInput === senhaCorreta) {
-    document.getElementById("entrar-conta").style.display = "none"
-    document.getElementById("div-cor-background").style.display = "none";
+    displayNoneForm.style.display = "none"
+    displayNoneBackground.style.display = "none";
     
   } else{
-    document.getElementById('senha-incorreta').innerHTML = 'Senha Incorreta, Por favor tente novamente. Caso se esqueceu da senha, aperte "esqueci a senha"';
+    var mensagemSenhaIncorreta = document.getElementById('senha-incorreta');
+    mensagemSenhaIncorreta.innerHTML = 'Senha Incorreta, Por favor tente novamente. Caso se esqueceu da senha, aperte "esqueci a senha"';
   }
 
 
